1f096a7623e1605a7eb06b0c06aab8191a2256ec
LazyNoodles <destroyer99988@gmail.com>
2024-05-20 16:04:31 +0700

    Update Hello Correct

--- hello.js
+++ hello.js
@@ -1,12 +1,10 @@
-const http = require ( 'http' );
-const hostname = 'localhost' ;
-const port = 3000 ;
-const server = http.createServer((req, res) => {
-	res.statusCode = 200 ;
-	res.setHeader( 'Content-Type' , 'text/plain' );
-	res.end( 'Hello World ' );
-});
+const express = require('express')
+const app = express()
 
-server.listen(port, hostname, () => {
-	console .log( `Server running at http://${hostname}:${port}/` );
-});
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
